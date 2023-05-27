2be7fe560eead21dd8780026df9a10347a75f87e
Leonardo Geja <leonardo.geja@dotgroup.com.br>
2023-05-27 13:15:33 -0300

    change service and controller for user.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
